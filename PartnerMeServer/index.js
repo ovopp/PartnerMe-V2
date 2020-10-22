55385163ce3670438f193c5c2a3a9a041a8afe13
Vincent <vincentyan8@hotmail.com>
2020-10-21 18:03:29 -0700

    added body parsing

--- PartnerMeServer/index.js
+++ PartnerMeServer/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 var createError = require('http-errors');
+var logger = require('morgan');
 
 app.use(express.json());
 
@@ -10,22 +11,34 @@ app.get('/', (request, response) => {
     response.send('Hello');
 });
 
-// AUTH SERVICE
+//////// AUTH SERVICE ////////
 app.get('/auth/getID', (request, response) => {
     response.send('Your ID');
 });
 
+/**
+    {
+    "user_id":"hello",
+    "password": "password"
+    }
+*/
+
 app.post('/auth/authorize', (request, response)=>{
+    // check user_id and password to make sure hash is correct.
     console.log(request.body);
     response.send("authorize");
 });
 
+
 app.post('/auth/external', (request, response)=>{
+    // check with fb / google auth
     console.log(request.body);
     response.send("external services");
 });
 
 app.post('/auth/create', (request, response)=>{
+    // check is user_id is already in db, return error
+    // connect to auth db and update with user_id and password
     console.log(request.body);
     response.send("create new account");
 });
@@ -54,5 +67,8 @@ app.use(function(err, req, res, next){
     return res.status(err.status || 500);
 });
 
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
