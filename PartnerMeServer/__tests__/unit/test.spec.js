49dafaf22bd10e7a111655f7004a75e448d9ab2a
Grady Thompson <grady_thompson@outlook.com>
2020-11-16 23:55:14 -0800

    Added all tests for two big modules

--- PartnerMeServer/__tests__/unit/test.spec.js
+++ PartnerMeServer/__tests__/unit/test.spec.js
@@ -1,15 +1,18 @@
 const backend = require("../../index");
+jest.mock("../../SQLquery") //Unsure about this line
 
 block //To stop test from running prematurely (leave error in until ready)
 
+//Hello world function
 describe("App get", () => {
     it("Should attempt to run our 'hello world' function as test", async () => {
-        const data = await backend.request(app).get();
+        const data = await backend.request(app).get('/');
 	expect(data.body == "Hello").toBeTruth();
     });
 });
 
 
+//Users
 describe("App get dbproxy test", () => {
     it("Should attempt to run a select query on the db", async () => {
 	const query = "SELECT name FROM users";
@@ -25,8 +28,8 @@ describe("App post user update #1", () => {
 		       "language":"",
 		       "availability":"",
 		       "hobbies":""};
-        const data = await backend.request(app).get('/dbproxy').send(query);
-	expect(data.body == "Example row for query: ${query}").toBeTruth();
+        const data = await backend.request(app).post('/user/update').send(input);
+	expect(data.body == "invalid json object").toBeTruth();
     });
 });
 
@@ -38,7 +41,134 @@ describe("App post user update #2", () => {
 		       "availability":"test",
 		       "hobbies":"test",
 		       "email":"test"};
-        const data = await backend.request(app).get('/dbproxy').send(query);
+        const data = await backend.request(app).post('/user/update').send(input);
+	expect(data.body == "success").toBeTruth();
+    });
+});
+
+describe("App post user current user #1", () => {
+    it("Should return with an error", async () => {
+	const input = {"name":"",
+		       "class":"",
+		       "language":"",
+		       "availability":"",
+		       "hobbies":""};
+        const data = await backend.request(app).post('/user/curren-user').send(input);
+	expect(data.message == "invalid json object").toBeTruth();
+    });
+});
+
+describe("App post user current user #2", () => {
+    it("Should return with success", async () => {
+	const input = {"name":"test",
+		       "class":"test",
+		       "language":"test",
+		       "availability":"test",
+		       "hobbies":"test",
+		       "email":"test"};
+        const data = await backend.request(app).post('/user/current-user').send(input);
+	expect(data.body == "success").toBeTruth();
+    });
+});
+
+//Authentication
+describe("App post auth check #1", () => {
+    it("Should return with an error", async () => {
+	const input = {"name":"",
+		       "class":"",
+		       "language":"",
+		       "availability":"",
+		       "hobbies":""};
+        const data = await backend.request(app).post('/auth/check').send(input);
+	expect(data.message == "invalid json object").toBeTruth();
+    });
+});
+
+describe("App post auth check #2", () => {
+    it("Should return with success", async () => {
+	const input = {"name":"test",
+		       "class":"test",
+		       "language":"test",
+		       "availability":"test",
+		       "hobbies":"test",
+		       "email":"test"};
+        const data = await backend.request(app).post('/auth/check').send(input);
+	expect(data.body == "success").toBeTruth();
+    });
+});
+
+describe("App post auth create #1", () => {
+    it("Should return with an error", async () => {
+	const input = {"name":"",
+		       "class":"",
+		       "language":"",
+		       "availability":"",
+		       "hobbies":""};
+        const data = await backend.request(app).post('/auth/create').send(input);
+	expect(data.message == "invalid json object").toBeTruth();
+    });
+});
+
+describe("App post auth create #2", () => {
+    it("Should return with success", async () => {
+	const input = {"name":"test",
+		       "class":"test",
+		       "language":"test",
+		       "availability":"test",
+		       "hobbies":"test",
+		       "email":"test"};
+        const data = await backend.request(app).post('/auth/create').send(input);
+	expect(data.body == "success").toBeTruth();
+    });
+});
+
+describe("App post auth get user #1", () => {
+    it("Should return with an error", async () => {
+	const input = {"name":"",
+		       "class":"",
+		       "language":"",
+		       "availability":"",
+		       "hobbies":""};
+        const data = await backend.request(app).post('/auth/getuser').send(input);
+	expect(data.message == "invalid json object").toBeTruth();
+    });
+});
+
+describe("App post auth get user #2", () => {
+    it("Should return with success", async () => {
+	const input = {"name":"test",
+		       "class":"test",
+		       "language":"test",
+		       "availability":"test",
+		       "hobbies":"test",
+		       "email":"test"};
+        const data = await backend.request(app).post('/auth/getuser').send(input);
+	expect(data.body == "success").toBeTruth();
+    });
+});
+
+//Matching service
+describe("App post matching get match #1", () => {
+    it("Should return with an error", async () => {
+	const input = {"name":"",
+		       "class":"",
+		       "language":"",
+		       "availability":"",
+		       "hobbies":""};
+        const data = await backend.request(app).post('/matching/getmatch').send(input);
+	expect(data.message == "invalid json object").toBeTruth();
+    });
+});
+
+describe("App post matching get match #2", () => {
+    it("Should return with success", async () => {
+	const input = {"name":"test",
+		       "class":"test",
+		       "language":"test",
+		       "availability":"test",
+		       "hobbies":"test",
+		       "email":"test"};
+        const data = await backend.request(app).post('/matching/getmatch').send(input);
 	expect(data.body == "success").toBeTruth();
     });
 });
