7b8549acb3bcdc00def6ab03f802e575972425e7
Grady Thompson <grady_thompson@outlook.com>
2020-11-16 18:09:00 -0800

    Added asynch to test

--- PartnerMeServer/__tests__/unit/m7test.spec.js
+++ PartnerMeServer/__tests__/unit/m7test.spec.js
@@ -1,15 +1,11 @@
 const backend = require("../../index");
 
-const testFunc = jest.fn(x => x === 1);
+const testFunc = jest.fn(x => x == 1);
 
 describe("test()", () => {
-    it("should return the value of the inputed function given an input value of 1", done => {
-	try {
-            expect(backend.test(testFunc)).toBeTruthy();
-	    done();
-	} catch (error) {
-	    done(error);
-	}
+    it("should return the value of the inputed function given an input value of 1", async () => {
+        const data = backend.test(testFunc);
+	expect(data).toBeTruthy();
     });
     
 });
