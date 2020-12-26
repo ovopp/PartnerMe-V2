61a8cccb100a28910d38e7bdf4703ae6e61fbedc
Vincent <vincentyan8@hotmail.com>
2020-12-26 00:43:31 -0800

    fixing endpoints

--- PartnerMeServer/functions.js
+++ PartnerMeServer/functions.js
@@ -1,6 +1,6 @@
 var similarity = require("compute-cosine-similarity");
 
-function cosineSim(req, user_list){
+function cosineSim(req, user_list) {
   var return_list = [];
   var return_user_list = [];
   var user_hobby_list = [];
@@ -9,78 +9,79 @@ function cosineSim(req, user_list){
    * 2) In the item list, do what we did before
    * 3) Profit
    */
-  for(var i = 0; i < user_list.length; i++){
-    if(req.body.Email == user_list[i].Email){
+  console.log(user_list);
+  for (var i = 0; i < user_list.length; i++) {
+    if (req.body.Email == user_list[i].Email) {
       user_hobby_list = user_list[i].Hobbies.split(", ");
     }
-    else{
-    var item = {
-      "Name" :user_list[i].Name,
-      "Class" : user_list[i].Class ,
-      "Language" : user_list[i].Language,
-      "Availability" : user_list[i].Availability,
-      "Hobbies" :user_list[i].Hobbies,
-      "Email" : user_list[i].Email
-    };
-    return_user_list.push(item);
+    else {
+      var item = {
+        "Name": user_list[i].Name,
+        "Class": user_list[i].Class,
+        "Language": user_list[i].Language,
+        "Availability": user_list[i].Availability,
+        "Hobbies": user_list[i].Hobbies,
+        "Email": user_list[i].Email
+      };
+      return_user_list.push(item);
     }
   }
 
-  for(let i = 0; i < return_user_list.length ; i++){
+  for (let i = 0; i < return_user_list.length; i++) {
     var otherUserList = return_user_list[i].Hobbies.split(", ");
     var userHobbyListTmp = user_hobby_list;
-    
+
     // initialize the dictionaries
     var dictionary = {};
     var tmpdict = {};
     // add keys from the lists to the dictionary
-    for(let j = 0; j < otherUserList.length ; j++){
-      if(!(otherUserList[j] in dictionary)){
+    for (let j = 0; j < otherUserList.length; j++) {
+      if (!(otherUserList[j] in dictionary)) {
         dictionary[otherUserList[j]] = 0;
         tmpdict[otherUserList[j]] = 0;
       }
     }
-    for(let j = 0; j < userHobbyListTmp.length ; j++){
-      if(!(userHobbyListTmp[j] in dictionary)){
+    for (let j = 0; j < userHobbyListTmp.length; j++) {
+      if (!(userHobbyListTmp[j] in dictionary)) {
         dictionary[userHobbyListTmp[j]] = 0;
         tmpdict[userHobbyListTmp[j]] = 0;
       }
     }
     // populate the dictionary to both dicts from the words from the list (vectors)
-    for(let j = 0; j < otherUserList.length; j++){
+    for (let j = 0; j < otherUserList.length; j++) {
       dictionary[otherUserList[j]] += 1;
     }
-    for(let j = 0; j < userHobbyListTmp.length; j++){
+    for (let j = 0; j < userHobbyListTmp.length; j++) {
       tmpdict[userHobbyListTmp[j]] += 1;
     }
     // empty the lists to be used to hold the values of the dict.
     otherUserList = [];
     userHobbyListTmp = [];
-    for(const key of Object.keys(dictionary)){
-      if(key != undefined){
+    for (const key of Object.keys(dictionary)) {
+      if (key != undefined) {
         otherUserList.push(dictionary[key]);
       }
     }
-    for(const key of Object.keys(tmpdict)){
-        userHobbyListTmp.push(tmpdict[key]);
+    for (const key of Object.keys(tmpdict)) {
+      userHobbyListTmp.push(tmpdict[key]);
     }
-    return_list.push({"similarity" : similarity(otherUserList, userHobbyListTmp), "userList": return_user_list[i]});
+    return_list.push({ "similarity": similarity(otherUserList, userHobbyListTmp), "userList": return_user_list[i] });
   }
   return_list.sort(compare);
   return return_list;
 }
 
-function compare( a, b ) {
-  if ( a.similarity > b.similarity ){
+function compare(a, b) {
+  if (a.similarity > b.similarity) {
     return -1;
   }
-  if ( a.similarity < b.similarity ){
+  if (a.similarity < b.similarity) {
     return 1;
   }
-    return 0;
+  return 0;
 }
 
 module.exports = {
-  compare : compare,
-  cosineSim : cosineSim
+  compare: compare,
+  cosineSim: cosineSim
 };
